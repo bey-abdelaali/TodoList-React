41d8e900249cefcd6206ab69d9a51d41de544bf9
bey abdelaali <bey.abdelaali@gmail.com>
2025-07-21 13:26:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoList from "./components/TodoList";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { TodosContext } from "./contexts/TodosContext";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { ToastProvider } from "./contexts/ToastContext";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Alexandria"],
+  },
+
+  palette: {
+    primary: {
+      main: "#dd2c00",
+    },
+  },
+});
+
+const initialTodos = [
+  {
+    id: uuidv4(),
+    title: "قراءة كتاب",
+    details: "تيسمبتيس يتسبميتس بيمستب",
+    isCompleted: false,
+  },
+  {
+    id: uuidv4(),
+    title: "قراءة كتاب",
+    details: "تيسمبتيس يتسبميتس بيمستب",
+    isCompleted: false,
+  },
+  {
+    id: uuidv4(),
+    title: "قراءة كتاب",
+    details: "تيسمبتيس يتسبميتس بيمستب",
+    isCompleted: false,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <ToastProvider>
+        <div
+          className="App"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "#191b1f",
+            height: "100vh",
+            direction: "rtl",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <TodosContext.Provider value={{ todos, setTodos }}>
+            <TodoList />
+          </TodosContext.Provider>
+        </div>
+      </ToastProvider>
+    </ThemeProvider>
   );
 }
 
